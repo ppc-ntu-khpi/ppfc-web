a8398f834eb9d18e82093ae80e384a2a9a350cea
VitaliiKozyrUA <vitalii.kozyr.ua@gmail.com>
2023-06-17 00:23:02 +0300

    Update: override equals and hashCode methods

--- src/jsMain/kotlin/tables/domain/model/Id.kt
+++ src/jsMain/kotlin/tables/domain/model/Id.kt
@@ -11,6 +11,17 @@ sealed class Id(val value: Long) {
     object Empty : Id(value = -1)
     class Value(value: Long) : Id(value = value)
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Id) return false
+
+        return value == other.value
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+
     companion object {
 
         private val random = Random(Date().getMilliseconds())
